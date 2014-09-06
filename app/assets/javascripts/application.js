7e965f64e886ad5586ef534a6c90d2e76cafa773
Nathan Hall <nathan.mh@gmail.com>
2014-09-05 21:56:52 -0500

    fixed it, reversed order jquery and coocoon were called in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require cocoon
 //= require jquery
 //= require jquery_ujs
+//= require cocoon
 //= require_tree .
