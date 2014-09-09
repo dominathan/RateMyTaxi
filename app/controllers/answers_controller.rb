45fc718bd96680383d23651db943d98ab30f4911
Nathan Hall <nathan.mh@gmail.com>
2014-09-09 09:24:54 -0500

    custom navbar with collapse

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,13 @@
 class AnswersController < ApplicationController
 
   def new_answer_review
-    if Taxi.find(params[:search])
+    begin
+      Taxi.find(params[:search]) #need to look at try and catch, throughs and error on taxi.find if not ther
       @taxi = Taxi.find(params[:search])
       @review = @taxi.user.reviews.last #most recent review? only allow one review? allow user to pick review?
       @questions = @review.questions.to_a.sort
       @answer = Answer.new
-    else
+    rescue
       flash[:danger] = "Taxi not found. Please use the 6 digit code."
       redirect_to root_path
     end
@@ -22,8 +23,7 @@ class AnswersController < ApplicationController
       answer.question_id = an['question_id']
       answer.save
     end
-    @review = @taxi.user.reviews.last #most recent review? only allow one review? allow user to pick review?
-    @questions = @review.questions.to_a.sort
+    binding.pry
     redirect_to root_path
   end
 
