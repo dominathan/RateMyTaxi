5fed62c04b92c0cb5361078c26bf5d534e01b95f
Nathan Hall <nathan.mh@gmail.com>
2014-09-15 10:53:30 -0500

    forgot to remove qr codes

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -1,11 +1,9 @@
 class QrCodesController < ApplicationController
 
   def create_qr_code
-    binding.pry
     @taxi = Taxi.find(params[:id])
     qr_code_leave_review_path = root_url + "survey/new?utf8=✓&search=" + params[:id].to_s
     @qr = nil
-    binding.pry
     qr_size = 3
     while @qr == nil && qr_size < 10
       begin
